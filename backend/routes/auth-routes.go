f10a0181cbb39d6b706a5ac91319775062c198dd
manasseh-fikadu <minase1047@gmail.com>
2024-03-12 19:35:10 +0300

    Company auth routes added

--- backend/routes/auth-routes.go
+++ backend/routes/auth-routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	company "example.com/agenagn/controllers/auth/company"
 	job_seeker "example.com/agenagn/controllers/auth/job-seeker"
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +13,11 @@ func SetupAuthRoutes(router *gin.Engine) {
         {
             job_seekerGroup.POST("/signup", job_seeker.SignUp)
             job_seekerGroup.POST("/login", job_seeker.Login)			
+        };
+        companyGroup := authGroup.Group("/company")
+        {
+            companyGroup.POST("/signup", company.CompanySignUp)
+            companyGroup.POST("/login", company.CompanyLogin)
         }
     }
 }
